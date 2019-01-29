d5c634e7161c8cd2dd76eb3c264822a1768a53a1
Harrison Lucas <harrisonwlucas@gmail.com>
2019-01-29 23:42:58 +1000

    Implemented task-item component & cleaned up lists

--- Frontend/src/app/task-container/task-list-item/task-list-item.component.ts
+++ Frontend/src/app/task-container/task-list-item/task-list-item.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { TaskDetail } from './../../shared/task-detail.model';
+
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-task-list-item',
   templateUrl: './task-list-item.component.html',
   styleUrls: ['./task-list-item.component.css']
 })
+
 export class TaskListItemComponent implements OnInit {
+  @Input() taskItem: TaskDetail;
 
   constructor() { }
 
